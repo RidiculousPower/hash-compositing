1557130f7b8d071555eb02ca300c59e21e8e09e2
Asher Haig <asher@ridiculouspower.com>
2013-07-08 04:04:31 -0400

    removed module-cluster dependency

--- lib/hash/compositing.rb
+++ lib/hash/compositing.rb
@@ -12,6 +12,50 @@ require_relative './requires.rb'
 
 class ::Hash::Compositing < ::HookedHash
 
+  ###################################  Non-Cascading Behavior  ####################################
+
+  #########################
+  #  non_cascading_store  #
+  #########################
+
+  ###
+  # @method non_cascading_store( key, object )
+  #
+  # Perform Hash#[]= without cascading to children.
+  #
+  # @param [Object] key
+  #
+  #        Key for store.
+  #
+  # @param [Object] object
+  #
+  #        Object to set at key.
+  #
+  # @return [Object]
+  #
+  #         Object set.
+  #
+  alias_method :non_cascading_store, :[]=
+
+  ##########################
+  #  non_cascading_delete  #
+  ##########################
+
+  ###
+  # @method non_cascading_delete( key, object )
+  #
+  # Perform Hash#delete without cascading to children.
+  #
+  # @param [Object] Key
+  #
+  #        Key for delete.
+  #
+  # @return [Object]
+  #
+  #         Object set.
+  #
+  alias_method :non_cascading_delete, :delete
+
   include ::Hash::Compositing::HashInterface
   
 end
--- lib/hash/compositing/do_not_inherit.rb
+++ lib/hash/compositing/do_not_inherit.rb
@@ -1,5 +1,8 @@
 # -*- encoding : utf-8 -*-
 
+###
+# Object used as unique constant to identify data that should not be inherited at cascade time.
+#
 module ::Hash::Compositing::DoNotInherit
   
 end
